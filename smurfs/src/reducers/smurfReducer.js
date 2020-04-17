5f874b6e0fcbf24f8b983d522af1309d483a9efc
unknown <jethanhargrave@gmail.com>
2020-04-17 13:27:01 -0400

    smurfin, data rendering - next add smurf form

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,3 +1,9 @@
+import {
+    FETCH_SMURFS_START,
+    FETCH_SMURFS_SUCCESS,
+    FETCH_SMURFS_FAIL,
+} from "../actions"
+
 const initialState = {
     smurfs: [],
     fetchingSmurfs: false,
@@ -6,6 +12,31 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
 
+    switch (action.type){
+        case FETCH_SMURFS_START:
+            return{
+                ...state,
+                fetchingSmurfs: true,
+            };
+        case FETCH_SMURFS_SUCCESS:
+            return {
+                ...state,
+                fetchingSmurfs: false,
+                smurfs: action.payload,
+                error: ""
+            }
+        case FETCH_SMURFS_FAIL:
+            return {
+                ...state,
+                fetchingSmurfs: false,
+                error: action.payload
+            }
+        default: 
+            return state;
+        }
+
 }
+            
+     
 
 export default reducer;
