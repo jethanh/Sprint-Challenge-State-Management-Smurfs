5d6ebebcadb92c704837a9ad379c77995a283f46
unknown <jethanhargrave@gmail.com>
2020-04-17 13:15:00 -0400

    reducer, actions set up, working on component

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,7 +6,8 @@ import App from "./components/App";
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from "redux-thunk";
-import reducer from "./reducers"
+import reducer from "./reducers";
+import logger from "redux-logger";
 
 const store = createStore(reducer, applyMiddleware(thunk, logger))
 
